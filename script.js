53480b2d8df8655a71285dfb1fea582642f440d7
Miho F <85437491+funamioh@users.noreply.github.com>
2022-01-04 18:14:43 +0900

    Modify tweetQuote function

--- script.js
+++ script.js
@@ -1,46 +1,48 @@
-const quote = document.getElementById('quote')
-const quoteBtn = document.getElementById('quoteBtn')
-const author = document.getElementById('author')
+const quote = document.getElementById("quote");
+const quoteBtn = document.getElementById("quoteBtn");
+const author = document.getElementById("author");
 
 let url = "https://type.fit/api/quotes";
 
-quoteBtn.addEventListener('click', generateQuote)
+quoteBtn.addEventListener("click", generateQuote);
 
-generateQuote()
+generateQuote();
 
 function generateQuote() {
-    const config = {
-      headers: {
-        Accept: 'application/json',
-      },
-    }
-    fetch(url, config)
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(data) {
-    let number = Math.floor(Math.random() * data.length);
-    console.log(data[number])
-    const quoteText = data[number].text;
-    const quoteAuthor = data[number].author;
-    quote.innerHTML = quoteText;
-    if (quoteAuthor === null) {
-      author.innerHTML = "- " + "Anonymous" + " -";
-    } else {
-    author.innerHTML = "- " + quoteAuthor + " -";
-    }
-  })  
-  .catch(function(error){
-  //if there is an error
-  });
+  const config = {
+    headers: {
+      Accept: "application/json",
+    },
+  };
+  fetch(url, config)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      let number = Math.floor(Math.random() * data.length);
+      console.log(data[number]);
+      const quoteText = data[number].text;
+      const quoteAuthor = data[number].author;
+      quote.innerHTML = quoteText;
+      if (quoteAuthor === null) {
+        author.innerHTML = "- " + "Anonymous" + " -";
+      } else {
+        author.innerHTML = "- " + quoteAuthor + " -";
+      }
+    })
+    .catch(function (error) {
+      //if there is an error
+    });
 }
 
-const tweetBtn = document.getElementById('tweetBtn')
+const tweetBtn = document.getElementById("tweetBtn");
 
-tweetBtn.addEventListener('click', shareTweet)
+tweetBtn.addEventListener("click", shareTweet);
 
-shareTweet()
+shareTweet();
 
 function shareTweet(event) {
- window.open("https://twitter.com/intent/tweet?text=" + quote + " -" + author);
-};
+  const twitterUrl =
+    "https://twitter.com/intent/tweet?text=${quoteText} -${quoteAuthor}";
+  window.open(twitterUrl, "_blank");
+}
